e54b211a9864c425d2a7d61df684bf56230fc348
hcolina <slushball420@gmail.com>
2020-09-23 14:19:11 -0500

    commit

---
+++ AnimatedGradient/AnimatedGradient/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+[assembly: ExportFont("ForumRegular.ttf", Alias="Forum")]
+namespace AnimatedGradient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new string[] { "Brush_Experimental" });
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
